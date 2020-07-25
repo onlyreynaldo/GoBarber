0b2ec253c7ac5265e8f676bd18f730e9b9e2e712
Eduardo Oliva <eduardok07@hotmail.com>
2020-07-25 20:42:36 -0300

    Adding celebrate to validate fields

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 
@@ -14,7 +14,13 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post('/', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required()
+  }
+}), usersController.create);
 
 usersRouter.patch(
   '/avatar',
