f7f3cb8a0be7f12c49334731edece87e5a9e8398
Eduardo Oliva <eduardok07@hotmail.com>
2020-08-02 20:20:26 -0300

    Adding Amazon S3 to upload files

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,21 +1,48 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tempFolder = path.resolve(__dirname, '../', '../', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage:StorageEngine
+  }
+  config : {
+    disk: {
+    };
+    aws: {
+      bucket: string;
+    }
+  }
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder: tempFolder,
   uploadsFolder: path.resolve(tempFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
 
-      const fileName = `${fileHash}-${file.originalname}`;
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
-    },
-  }),
-};
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-oliva'
+    }
+  }
+} as IUploadConfig;
